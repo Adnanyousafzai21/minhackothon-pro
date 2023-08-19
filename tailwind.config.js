03d09f1bd12077b130f38e2ca63387634c068398
Adnan Rafiq <91646991+Adnanyousafzai21@users.noreply.github.com>
2023-08-19 21:02:32 +0500

    myfrist commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        custom: '#7749F8', 
+      },
+    },
+  },
+  plugins: [],
+}
+
